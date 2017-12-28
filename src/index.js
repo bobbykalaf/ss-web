60286ac997829012ad3e6561e2e4602f0235959e
Bobby Kalaf <bobby.kalaf@gmail.com>
2017-12-28 01:47:32 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/index.css';
+import AppRoot from './AppRoot/AppRoot.react';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<AppRoot />, document.getElementById('root'));
+registerServiceWorker();
